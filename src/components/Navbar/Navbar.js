2bce180aecb819640e17c1b1c555bb9c5e5e8e73
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2020-12-23 01:10:29 +0545

    create issue modal added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,46 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Button from 'react-bootstrap/Button';
 
 import RoundAvatar from '../common/RoundAvatar/RoundAvatar';
+import CreateIssue from '../Issue/CreateIssue';
 
 import '../../assets/scss/navbar.scss';
 import image from '../../assets/images/jira.png';
 
 const Navbar = () => {
   const { user, logout } = useAuth0();
+  const [showCreateModal, setShowCreateModal] = useState(false);
 
   return (
-    <div className="navbar-container">
-      <section>
-        <ul>
-          <li>
-            <div className="logo-wrapper">
-              <img src={image} alt="logo"></img>
-            </div>
-          </li>
-          <li>Your work</li>
-          <li>Filter</li>
-          <li>Dashboard</li>
-          <li>
-            <Button>Create</Button>
-          </li>
-        </ul>
-      </section>
-
-      <section className="d-flex">
-        <Button variant="light" onClick={logout}>
-          logout
-        </Button>
-        <ul>
-          {user ? (
+    <>
+      <div className="navbar-container">
+        <section>
+          <ul>
             <li>
-              <RoundAvatar image={user.picture}></RoundAvatar>
+              <div className="logo-wrapper">
+                <img src={image} alt="logo"></img>
+              </div>
             </li>
-          ) : null}
-        </ul>
-      </section>
-    </div>
+            <li>Your work</li>
+            <li>Filter</li>
+            <li>Dashboard</li>
+            <li>
+              <Button onClick={() => setShowCreateModal(true)}>Create</Button>
+            </li>
+          </ul>
+        </section>
+
+        <section className="d-flex">
+          <Button variant="light" onClick={logout}>
+            logout
+          </Button>
+          <ul>
+            {user ? (
+              <li>
+                <RoundAvatar image={user.picture}></RoundAvatar>
+              </li>
+            ) : null}
+          </ul>
+        </section>
+      </div>
+      <CreateIssue
+        onHide={() => setShowCreateModal(false)}
+        show={showCreateModal}
+      />
+    </>
   );
 };
 
