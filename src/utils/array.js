78fd93ea4c55e6df701787496af10da71f73e509
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2020-12-24 00:51:04 +0545

    task fetch intreation and mapping

--- src/utils/array.js
+++ src/utils/array.js
@@ -16,3 +16,22 @@ export const convertObjectIntoArray = (obj) => {
 export const isArrayEmpty = (arr) => {
   return arr.length === 0;
 };
+
+export const convertArrayToObject = (array, key, swimeLine) => {
+  const initialValue = {};
+
+  let finalValue = {};
+
+  swimeLine.forEach((line) => {
+    const extractedObj = array.reduce((obj, item) => {
+      return {
+        ...obj,
+        [line]: array.filter((list) => list.status === line) || [],
+      };
+    }, initialValue);
+
+    finalValue = Object.assign(finalValue, extractedObj);
+  });
+
+  return finalValue;
+};
